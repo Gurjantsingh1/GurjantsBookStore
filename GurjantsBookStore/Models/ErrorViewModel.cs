b5c109552223883d42fb0cd2c353af587f125c26
W0782841 <W0782841@ADSOL0171C-18>
2022-04-06 16:21:03 -0400

    Add project files.

---
+++ GurjantsBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GurjantsBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
